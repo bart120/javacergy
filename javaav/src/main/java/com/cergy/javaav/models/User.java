8b84f800039bb0725663c9ebce4751e34dcdba83
Vincent <vleclerc@inow.fr>
2021-11-22 12:03:09 +0100

    model

---
+++ javaav/src/main/java/com/cergy/javaav/models/User.java
@@ -0,0 +1,49 @@
+package com.cergy.javaav.models;
+
+public class User {
+    private long id;
+    private String lastname;
+    private String firstname;
+    private String email;
+    private String phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
